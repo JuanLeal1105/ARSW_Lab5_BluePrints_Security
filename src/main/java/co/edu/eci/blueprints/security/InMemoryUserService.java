6994658dcb52d9228475d32573aca63484203457
JuanLeal1105 <JUAN.LCRUZ@mail.escuelaing.edu.co>
2026-02-26 00:23:46 -0500

    feat: Se añade una función para obtener el scope que luego se llama en el AuthController

--- src/main/java/co/edu/eci/blueprints/security/InMemoryUserService.java
+++ src/main/java/co/edu/eci/blueprints/security/InMemoryUserService.java
@@ -14,11 +14,26 @@ public class InMemoryUserService {
         this.users = Map.of(
             "student", encoder.encode("student123"),
             "assistant", encoder.encode("assistant123")
+                , "admin", encoder.encode("admin123")
         );
     }
 
     public boolean isValid(String username, String rawPassword) {
         String hash = users.get(username);
         return hash != null && encoder.matches(rawPassword, hash);
+    }
+
+    public String getScopeByUsername (String userCase){
+        switch (userCase){
+            case "admin":
+                return "blueprints.read blueprints.write blueprints.create blueprints.update";
+            case "student":
+                return "blueprints.read";
+            case "assistant":
+                return "blueprints.read blueprints.write";
+            default:
+                return "";
+
+        }
     }
 }
